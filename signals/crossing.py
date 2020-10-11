c4bbaa89c17e40155dc4bf5006fb2577e3e3c132
Lorne <dev@luotao.net>
2020-10-12 09:43:39 +1100

    add signal corssing

--- signals/crossing.py
+++ signals/crossing.py
@@ -11,6 +11,24 @@ def check_cross(data1, data2, shift=1):
     return None
 
 
+def cross_over(fast_prices, slow_prices, shift=1):
+    """True if fast cross over slow"""
+    last_index = len(slow_prices) - 1 - shift
+    previous_index = last_index - 1
+    if fast_prices[last_index] > slow_prices[last_index] and fast_prices[previous_index] < slow_prices[previous_index]:
+        return True
+    return False
+
+
+def cross_under(fast_prices, slow_prices, shift=1):
+    """True if slow cross over fast"""
+    last_index = len(slow_prices) - 1 - shift
+    previous_index = last_index - 1
+    if fast_prices[last_index] < slow_prices[last_index] and fast_prices[previous_index] > slow_prices[previous_index]:
+        return True
+    return False
+
+
 def check_reverse(data, shift=1):
     first = -1 - shift
     second = -2 - shift
